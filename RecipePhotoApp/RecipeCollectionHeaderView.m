2722b447cd5a4e13863a3717fef8a72924f85f4a
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-28 14:44:18 -0400

    inserted Header

---
+++ RecipePhotoApp/RecipeCollectionHeaderView.m
@@ -0,0 +1,13 @@
+//
+//  RecipeCollectionHeaderView.m
+//  RecipePhotoApp
+//
+//  Created by Kunwardeep Gill on 2015-04-28.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import "RecipeCollectionHeaderView.h"
+
+@implementation RecipeCollectionHeaderView
+
+@end
