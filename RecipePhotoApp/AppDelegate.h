51d93d20f8ce202410605f7065a9b28baab4519f
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-28 08:31:10 -0400

    Initial Commit

---
+++ RecipePhotoApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RecipePhotoApp
+//
+//  Created by Kunwardeep Gill on 2015-04-28.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RecipePhotoApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RecipePhotoApp
+//
+//  Created by Kunwardeep Gill on 2015-04-28.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
