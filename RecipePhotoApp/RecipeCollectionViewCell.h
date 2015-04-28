e20d1ee9888b2f204b2926d6d5a30fe91198ce71
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-28 08:37:07 -0400

    set up the recipeCollectionViewCell Class

---
+++ RecipePhotoApp/RecipeCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  RecipeCollectionViewCell.h
+//  RecipePhotoApp
+//
+//  Created by Kunwardeep Gill on 2015-04-28.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ RecipePhotoApp/RecipeCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  RecipeCollectionViewCell.m
+//  RecipePhotoApp
+//
+//  Created by Kunwardeep Gill on 2015-04-28.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import "RecipeCollectionViewCell.h"
+
+@implementation RecipeCollectionViewCell
+
+@end
