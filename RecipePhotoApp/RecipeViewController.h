15674746ffd118bc52c8899588ae5f7ae061c94c
kunwardeep <kunwardeep.gill@gmail.com>
2015-04-30 13:44:17 -0400

    connected image view to recipesViewController

---
+++ RecipePhotoApp/RecipeViewController.h
@@ -0,0 +1,14 @@
+//
+//  RecipeViewController.h
+//  RecipePhotoApp
+//
+//  Created by Kunwardeep Gill on 2015-04-30.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecipeViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *recipeImageView;
+
+@end
---
+++ RecipePhotoApp/RecipeViewController.m
@@ -0,0 +1,37 @@
+//
+//  RecipeViewController.m
+//  RecipePhotoApp
+//
+//  Created by Kunwardeep Gill on 2015-04-30.
+//  Copyright (c) 2015 ProjectDGW. All rights reserved.
+//
+
+#import "RecipeViewController.h"
+
+@interface RecipeViewController ()
+
+@end
+
+@implementation RecipeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
